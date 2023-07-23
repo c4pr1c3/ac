4eeb1abfc0636aab667d29d600ed47955a24d6c4
于子涵 <1819302272@qq.com>
2023-07-23 14:50:01 +0800

    前端静态页面完善

--- src/views/register.py
+++ src/views/register.py
@@ -3,10 +3,10 @@ import re
 from form import RegisterForm
 
 register = Blueprint('register', __name__)
-#用户名要求：中文、英文字母、数字；-、_、.等合法字符集范围之外的字符不允许使用；长度限制在36个字符之内
-username_pattern = re.compile(r'[\u4e00-\u9fa5a-zA-Z0-9]{2,36}$s')
+#用户名要求：可包含中英文、数字、合法字符，长度限制2-36字符
+username_pattern = re.compile(r'[\u4e00-\u9fa5a-zA-Z0-9]{2,36}$')
 
-#密码要求
+#密码要求:必须包含大小写、数字，长度限制8-36字符
 password_pattern = re.compile(r'(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[\s\S]{8,36}')
 
 
@@ -25,7 +25,7 @@ def post__register():
         assert username_pattern.fullmatch(username), '用户名不合法！'
         password = form.password.data
         confirm_password = form.confirm_password.data
-        assert password == confirm_password, '两次输入密码不一致！'
+        assert password != confirm_password, '两次输入密码不一致！'
         assert password_pattern.fullmatch(password), '密码不合法！'
         hash_password = form.get_hash_password()
         User.create_user(username, hash_password)
